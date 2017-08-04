00d9543d2d03ed58f8203e78ee96f455cd039417
rmzoni <remi@gft.com>
2017-08-04 16:49:16 -0300

    Working with XSLT, validation and error handler in Camel

--- camel-traning/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-traning/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -2,6 +2,7 @@ package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -14,17 +15,36 @@ public class RotaPedidos {
 
 	            @Override
 	            public void configure() throws Exception {
+	            	
+	            	errorHandler(
+            		    deadLetterChannel("file:erro").
+            		        logExhaustedMessageHistory(true).
+            		        maximumRedeliveries(3).
+            		            redeliveryDelay(5000).
+            		        onRedelivery(new Processor() {            
+            		            @Override
+            		            public void process(Exchange exchange) throws Exception {
+            		                int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+            		                int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+            		                System.out.println("Redelivery - " + counter + "/" + max );
+            		            }
+            		        })
+            		);
+	            	
 	            	from("file:pedidos?delay=5s&noop=true").
 		                routeId("rota-pedidos").
+		                to("validator:pedido.xsd").
 		                multicast().
 		                	parallelProcessing().
 		                    	to("direct:soap").
 		                    	to("direct:http");
 	
-		            from("direct:soap").
+	            	from("direct:soap").
 		                routeId("rota-soap").
-		                log("chamando servico soap ${body}").
-		            to("mock:soap");
+		            to("xslt:pedido-para-soap.xslt").
+		                log("Resultado do template: ${body}").
+		                setHeader(Exchange.CONTENT_TYPE,constant("text/xml")).
+		            to("http4://localhost:8080/webservices/financeiro");
 	
 		            from("direct:http").
 		                routeId("rota-http").
